16b0624d246873dc6049729fa7210e411e14afdc
OMEN <youssraeh1997@gmail.com>
2021-12-20 16:16:25 +0100

    v2

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,10 +5,10 @@ import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 import { SearchComponent } from './search/search.component';
-import { InfoComponent } from './info/info.component';
+
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent, SearchComponent, InfoComponent],
+  declarations: [HomeComponent, SearchComponent],
 })
 export class HomeModule {}
